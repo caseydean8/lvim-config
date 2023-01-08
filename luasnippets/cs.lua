74680e4301255b24ea43882a801491c787f2b621
Casey Carroll <caseydean8@gmail.com>
2023-01-07 19:12:12 -0500

    Added C# boilerplate

--- luasnippets/cs.lua
+++ luasnippets/cs.lua
@@ -1,3 +1,27 @@
+-- local s = ls.snippet
+-- local t = ls.text_node
+-- local i = ls.insert_node
+
 return {
-  s("sd", { t "System.Diagnostics.Debug.WriteLine"}),
+  s("sd", { t "System.Diagnostics.Debug.WriteLine" }),
+
+  -- fmt() allows {} in first argument to be tabbed to correct position. 
+  s("Pro", fmt([[
+  {}
+  {}
+      {}
+      {}
+          {}
+      {}
+  {}
+ ]], {
+    t "class Program",
+    t "{",
+    t "public static void Main(String[] args)",
+    t "{",
+    -- insert cursor
+    i(1),
+    t "}",
+    t "}",
+  })),
 }
